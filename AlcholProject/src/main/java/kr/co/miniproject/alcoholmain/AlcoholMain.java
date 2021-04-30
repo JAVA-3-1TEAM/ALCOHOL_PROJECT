d1c6e12147a38585972639287c64d39c7157c09b
HanJeongSeol <hanjeong94@naver.com>
2021-04-30 11:32:23 +0900

    MenuScreen Edit

--- AlcholProject/src/main/java/kr/co/miniproject/alcoholmain/AlcoholMain.java
+++ AlcholProject/src/main/java/kr/co/miniproject/alcoholmain/AlcoholMain.java
@@ -1,10 +1,12 @@
 package kr.co.miniproject.alcoholmain;
 
+import java.util.Scanner;
+
 import kr.co.miniproject.menu.MenuScreen;
 
 public class AlcoholMain {
 	public static void main(String[] args) {
-		MenuScreen mc = new MenuScreen();
-		mc.mainScreen();
+		MenuScreen menu = new MenuScreen();
+		menu.mainScreen();
 	}
 }
